fb7536476cf3b40ad6fa8a69b0ddafa4ebe9aaf4
Rachel Isaac <rachelisaac995@gmail.com>
2023-06-07 19:31:42 +0400

    Set up user authentication

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,2 +1,3 @@
 class FavouritesController < ApplicationController
+  before_action :authenticate_user!
 end
--- test/models/user_favourite_test.rb
+++ test/models/user_favourite_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class UserFavouriteTest < ActiveSupport::TestCase
   # test "the truth" do
