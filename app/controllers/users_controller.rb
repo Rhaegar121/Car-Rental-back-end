6b91f7ac654b9b749fc00ca615007d8a570ca72a
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-08 23:06:48 +0630

    add authentication methods in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,25 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-    render json: @users
+  def login
+    @user = User.find_by(fullname: params[:fullname])
+    if @user
+      render json: @user, status: 200
+    else
+      render json: { errors: ['User not found'] }, status: 422
+    end
   end
 
-  def show
-    @user = User.find(params[:id])
-    render json: @user, status: :ok
+  def signup
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: 200
+    else
+      render json: { errors: @user.errors.full_messages }, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:fullname)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(fullname: "User", email: "user@gmail.com", password: "password")
+User.create(fullname: "User")
 Car.create(name: "Toyota Corolla", price: 1000, ratings: 5, image: "https://i.pinimg.com/564x/ac/06/2b/ac062bb62fe79b9642c73efdc6fbcdf8.jpg", description: "In this house, we love Toyotas. Especially the Corolla, which boasts top spots on virtually every “best rentals” list—and for good reason. With a generous 29 city / 37 highway miles to the gallon, 88.6 cubic feet of passenger space, and enough room to seat five passengers comfortably, it’s a capable choice for just about any situation.", user_id: 1)
 Car.create(name: "Toyota Prius", price: 800, ratings: 4, image: "https://i.pinimg.com/564x/e1/a2/ef/e1a2ef0f2f408561dece6e357f5fde3b.jpg", description: "Looking for a fuel-savvy traveler? There’s no better option than the Toyota Prius. With a renowned EPA rating of 54 city / 50 highway miles per gallon, you’ll be hard-pressed to find a rental that saves you more on gas money—especially in the city. Its hybrid powertrain isn’t just a fuel-sipper, though—with nimble steering and suspension, it’s also a breeze to drive.", user_id: 1)
 Favourite.create(user_id: 1, car_id: 2);
