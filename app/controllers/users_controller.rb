6f9238b6ca2d03d680dc3aa9865a239d10ca7df3
chaseknis <chase6th@gmail.com>
2023-06-08 14:43:25 +0200

    Update user.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,3 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  validates :name, presence: true, length: { maximum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, length: { maximum: 50 }
   has_many :cars
   has_many :favourites
 end
