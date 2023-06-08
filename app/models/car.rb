a2d37e7695ffa10f068f95f786b9cfc8b18e8229
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-09 00:02:28 +0630

    add unit tests for car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -5,6 +5,6 @@ class Car < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :ratings, presence: true, numericality: { greater_than_or_equal_to: 0 }, length: {minimum:0, maximum:5}
+  validates :ratings, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
   validates :image, presence: true
 end
