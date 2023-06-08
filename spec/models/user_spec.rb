8faa7f01ca7d75714a004152c982a8edf9069f73
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-08 23:57:11 +0630

    add unit tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'associations' do
-    it { should has_many(:cars) }
-    it { should has_many(:favourites) }
-  end
+  describe 'User Model' do
+    before do
+      @user = User.new(fullname: 'User')
+    end
 
-  describe 'validations' do
-    it { should validate_presence_of(:fullname) }
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:password) }
+    it 'is not valid without a name' do
+      @user.fullname = nil
+      expect(@user).to_not be_valid
+    end
+
+    it 'is valid with valid attributes' do
+      expect(@user).to be_valid
+    end
+
+    it 'is not valid with a name longer than 50 characters' do
+      @user.fullname = 'a' * 51
+      expect(@user).to_not be_valid
+    end
   end
 end
