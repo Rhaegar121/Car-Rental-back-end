7fc135dfce0b72169dd3c7e915589e1374024340
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-21 22:58:58 +0630

    update session storage for production environment

--- config/initializers/session_storage.rb
+++ config/initializers/session_storage.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://carrental-backend-jmdo.onrender.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
