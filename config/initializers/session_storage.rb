c6d3f95a4bc700291e4f24e8f757240a8953b7c3
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-08-31 23:11:55 +0630

    update domain url in session_storage.rb

--- config/initializers/session_storage.rb
+++ config/initializers/session_storage.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://carrental-backend-jmdo.onrender.com"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://carrental-9ijm.onrender.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end
