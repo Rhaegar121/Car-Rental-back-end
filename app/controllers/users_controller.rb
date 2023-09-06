8ae13896a7a54867cc5f6cfe2ae4bdcb99520cb9
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-09-06 21:16:30 +0630

    add update method in controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  def update
+    @user = User.find(params[:id])
+  
+    if @user.update(user_params)
+      render json: @user, status: :200
+    else
+      render json: { errors: @user.errors.full_messages }, status: :422
+    end
+  end
+
   def login
     @user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
 
@@ -29,5 +39,11 @@ class UsersController < ApplicationController
   def logout
     reset_session
     render json: { message: 'Logged out' }, status: 200
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:fullname, :icon)
   end
 end
